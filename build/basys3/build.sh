5306c5c66dd7640b0ba0620a9846fc4e93b8f627
hamsternz <hamster@snap.net.nz>
2020-12-18 12:28:34 +1300

    Added non-working template for basys3 build scripts

--- build/basys3/build.sh
+++ build/basys3/build.sh
@@ -1,1 +1,5 @@
-#Buildscript goes here
+###########################
+# Buildscript goes here
+###########################
+. /opt/Xilinx/Vivado/2019.2/settings64.sh
+echo source build.tcl | vivado -mode tcl
