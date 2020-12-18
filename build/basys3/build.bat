7d0ea6f38e3b938ce31493abe56ef78da3ab985c
Mike Field <hamster@snap.net.nz>
2020-12-18 13:19:10 +1300

    Added build.bat

---
+++ build/basys3/build.bat
@@ -0,0 +1,8 @@
+echo =======================================
+echo 
+echo Remeber to set up your vivado environment
+echo e.g. run:
+echo /Xilinx/Vivado/2019.2/settings64.bat
+echo
+echo =======================================
+echo source build.tcl | vivado -mode tcl
